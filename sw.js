740df3a6bd9bb6ebdc58e811d8b12a05692c3da0
ss-pixel1 <ss8540839@gmail.com>
2025-10-07 13:19:08 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_VERSION = 'v1.0.0';
+const CACHE_NAME = `love-match3-${CACHE_VERSION}`;
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      caches.match('./index.html').then(cached => cached || fetch('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const network = fetch(req).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        return resp;
+      }).catch(() => cached || Promise.reject('offline'));
+      return cached || network;
+    })
+  );
+});
